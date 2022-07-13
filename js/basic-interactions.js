eed7e17095082718a9681e253abac5f8e5b8529f
Lucas dos Santos <lucasmadarame@gmail.com>
2022-07-13 18:40:09 -0300

    efeitos visuais ao abrir menu hamburguer adicionado

--- js/basic-interactions.js
+++ js/basic-interactions.js
@@ -1,7 +1,9 @@
 let hamburguer = document.querySelector(".nav-primary-button");
 let primaryMenu = document.getElementsByClassName("primary-menu-control")[0];
+let headerCover = document.getElementById("header-cover");
 
 hamburguer.addEventListener("click", () => {
         primaryMenu.classList.toggle("primary-menu-control");
         hamburguer.classList.toggle("nav-primary-animation");
+        headerCover.classList.toggle("header-cover-image-effetcs");
 });

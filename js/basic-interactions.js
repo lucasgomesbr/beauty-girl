a5a19784c12e24567c0f0607ee124339f9ef3ca3
Lucas dos Santos <lucasmadarame@gmail.com>
2022-07-14 00:21:18 -0300

    estrutura inicial da section de informação criada

--- js/basic-interactions.js
+++ js/basic-interactions.js
@@ -1,9 +1,11 @@
 let hamburguer = document.querySelector(".nav-primary-button");
 let primaryMenu = document.getElementsByClassName("primary-menu-control")[0];
 let headerCover = document.getElementById("header-cover");
+let headerSectionInfo = document.getElementsByClassName("section-info")[0];
 
 hamburguer.addEventListener("click", () => {
         primaryMenu.classList.toggle("primary-menu-control");
         hamburguer.classList.toggle("nav-primary-animation");
         headerCover.classList.toggle("header-cover-image-effetcs");
+        headerSectionInfo.classList.toggle("header-section-effects");
 });

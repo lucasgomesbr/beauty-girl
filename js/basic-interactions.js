e3d21a6c88e29a687f66dd1a7928e9cc87a4a4f4
Lucas dos Santos <lucasmadarame@gmail.com>
2022-08-05 16:51:04 -0300

    melhoria na interatividade dos links do menu principal e ajustes na logo

--- js/basic-interactions.js
+++ js/basic-interactions.js
@@ -1,5 +1,7 @@
+// comportamentos do menu hamburguer
 let hamburguer = document.querySelector(".nav-primary-button");
 let primaryMenu = document.getElementsByClassName("primary-menu-control")[0];
+// boxMobile é o efeito de escurecimento dos elementos na tela ao abrir o menu hamburguer.
 let boxMobile = document.getElementById("the-box-mobile");
 
 hamburguer.addEventListener("click", () => {
@@ -7,3 +9,19 @@ hamburguer.addEventListener("click", () => {
         hamburguer.classList.toggle("nav-primary-animation");
         boxMobile.classList.toggle("box-transparent-mobile");
 });
+
+// Interatividade para os links do menu na versão desktop
+let menuItem = document.querySelectorAll('.itemMenu');
+let menuLength = menuItem.length;
+
+for (let i = 0; i < menuLength; i++) {    
+        menuItem[i].addEventListener("click", () => {
+                menuItem[0].classList.remove("menuDesktopClicked");
+                menuItem[1].classList.remove("menuDesktopClicked");
+                menuItem[2].classList.remove("menuDesktopClicked");
+                menuItem[3].classList.remove("menuDesktopClicked");
+                menuItem[4].classList.remove("menuDesktopClicked");
+                menuItem[i].classList.add("menuDesktopClicked");      
+        });
+} 
+
